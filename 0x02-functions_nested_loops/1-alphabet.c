a4767fec4d9149c57d4c913e12f70fa5bbcc2d9b
iakah <iakah2313@gmail.com>
2022-12-14 08:00:05 -0800

    alphabet 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -6,14 +5,12 @@
  * Description: It prints the word Holberton, followed by a new line.
  * Return: 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	char word[8] = "_putchar";
-	int i;
 
-	for (i = 0; i < 8; i++)
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
-
-	return (0);
 }
