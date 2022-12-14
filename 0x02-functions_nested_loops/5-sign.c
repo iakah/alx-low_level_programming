6e1aff7e4359a6370f8d2408e00e1031da15e768
Ian Kahare <37710249+iakah@users.noreply.github.com>
2022-12-15 00:40:51 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - Check
  * @n: An input number
- * Description: Tohis function prints the sign of the number
+ * Description: This function prints the sign of the number
  * Return: 1 if number is positive, o if number is 0, or
  * -1 if number is negative
  */
