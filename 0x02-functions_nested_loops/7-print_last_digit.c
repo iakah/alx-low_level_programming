623e2c006aa271d81fce163acad4b8ca768e7b4f
iakah <iakah2313@gmail.com>
2022-12-14 11:58:35 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - Check 
+ * print_last_digit - Check
  * @r: An integer input
  * Description: This function prints the last digit of a number
  * Return: last digit of number r
