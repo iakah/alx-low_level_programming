be6348788e8b572d66e1327569d1c54075ce1114
iakah <iakah2313@gmail.com>
2022-12-14 13:50:27 -0800

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_times_table - prints times table for numbers from 0-14
@@ -7,49 +7,34 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i, k;
 
 	if (n > 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
-			for (j = 1; j <= n; j++)
-				putformat(i * j);
-			_putchar('\n');
-		}
-	}
-}
+			k = n * i;
 
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-void putformat(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
+			if ((k / 10) == 0)
+			{
+				if (i != 0)
+					_putchar(' ');
+				_putchar(k + '0');
+
+				if (i == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (i == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
 	}
 }
