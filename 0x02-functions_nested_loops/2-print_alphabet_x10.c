3af73bbeb3641a1013ddc788c2c163e96ec7e292
iakah <iakah2313@gmail.com>
2022-12-14 08:37:57 -0800

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,16 @@
  * alphabet in lowercase 10 times
  * Return: Nothing.
  */
+
 void print_alphabet_x10(void)
 {
-	int j;
 	char i;
-	for (j = 0; j<10; j++)
+	int j;
+
+	for (j = 0; j < 10; j++)
 	{
-        	for (i = 'a'; i <= 'z'; i++)
-                	_putchar(i);
-        	_putchar('\n');
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
 	}
 }
