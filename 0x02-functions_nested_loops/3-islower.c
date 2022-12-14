db1472c53717fae9ccc566cadff6797b87bdbf7b
iakah <iakah2313@gmail.com>
2022-12-14 08:54:22 -0800

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _islower - 
+ * _islower - Check
  * @c: An input character
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
  */
-
 int _islower(int c)
 {
 	char i;
