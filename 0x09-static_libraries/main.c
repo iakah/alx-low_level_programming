21393b8c64f05964152784664bdff9a7fee2e5e2
iakah <iakah2313@gmail.com>
2023-01-09 10:37:39 -0800

    creating library Mitnick

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * function main - prints Kevin Mitnicks quote
+ * return 0- The function returns 0
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
