76e803ee68f959bbac113b6840eccc475dfac7da
iakah <iakah2313@gmail.com>
2023-01-09 13:35:33 -0800

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - takes argument to print name of executable
+ * @argc: the totl number of arguments
+ * @argv: the array arguments
+ * _putchar - print function
+ * Return - returns 0 (success)
+ */
+
+int main(int argc,char *argv[])
+{
+if(argc>0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
