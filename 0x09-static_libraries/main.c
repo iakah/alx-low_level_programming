f92f835c531a8ca7e37b0ba2449fbc07f27fbc00
iakah <iakah2313@gmail.com>
2023-01-09 10:42:39 -0800

    update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,10 +1,7 @@
 #include "main.h"
-/**
- * function main - prints Kevin Mitnicks quote
- * return 0- The function returns 0
- */
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+  _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+  return (0);
 }
