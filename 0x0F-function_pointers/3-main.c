f18b8228bfe038e63d5d0a252e0e25e6423326ce
iakah <iakah2313@gmail.com>
2023-01-25 14:24:14 -0800

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,55 +1,31 @@
+#include "3-calc.h"
 #include <stdio.h>
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
+ * main - program to perform simple operations
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: exits 0 normally, 98 on argc error, 99 on illegal operator
  */
-int is_98(int elem)
+int main(int argc, char *argv[])
 {
-  return (98 == elem);
-}
+	int (*func)(int, int);
 
-/**
- * is_strictly_positive - check if a number is greater than 0
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_strictly_positive(int elem)
-{
-  return (elem > 0);
-}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-/**
- * abs_is_98 - check if the absolute value of a number is 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
-{
-  return (elem == 98 || -elem == 98);
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-  int index;
-
-  index = int_index(array, 20, is_98);
-  printf("%d\n", index);
-  index = int_index(array, 20, abs_is_98);
-  printf("%d\n", index);
-  index = int_index(array, 20, is_strictly_positive);
-  printf("%d\n", index);
-  return (0);
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
